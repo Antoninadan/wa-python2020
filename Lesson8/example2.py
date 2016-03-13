a447c8165678e99ae64f6e2bc2f7a5e695663e9b
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-03-13 13:50:50 +0200

    Show example of correct exception order

--- Lesson8/example2.py
+++ Lesson8/example2.py
@@ -1,7 +1,7 @@
 try:
     print(2 / 0)
-except ArithmeticError:
-    print('arithmetic error occured')
 except ZeroDivisionError:
     print('division by zero')
+except ArithmeticError:
+    print('arithmetic error occured')
 
