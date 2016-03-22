83bd23d8bba960624513fd899dfb649f3ddb3051
Danilova Antonina <mail100@i.ua>
2016-03-22 18:31:44 +0200

    задание 1

--- Tasks-2016-03-13/task1.py
+++ Tasks-2016-03-13/task1.py
@@ -4,26 +4,30 @@ class Book:
 
     def __init__(self, title, year, genre):
         """Конструктор
-
         Задание: сохраните title, year и genre в виде
         атрибутов объекта self
         """
+        self.title = title
+        self.year = year
+        self.genre = genre
 
     def __repr__(self):
         """Возвращает внутреннее строковое представление объекта
         для консоли, отладчика и т.д.
-
         Задание: верните строку в формате
                  Book("название", год, "жанр")
         """
+        return 'Book("{!r}", {!r}, "{!r}")'.format(self.title, self.year, self.genre)
 
     def __str__(self):
         """Возвращает строку, предназначенную для пользователя
         (str, print и т.д.)
-
         Задание: верните строку в произвольном формате
         """
-
+        return '{!r} - {!r} ({!r})'.format(self.title, self.year, self.genre)
 
 # Задание: создайте несколько объектов класса Book
 # и выведите их на экран
+
+book1 = Book('War and peace', 2011, 'lirics')
+print(book1)
