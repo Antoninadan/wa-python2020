0179f5024bbb699031cc514a03bdaddbb9951245
Danilova Antonina <mail100@i.ua>
2016-03-27 01:58:59 +0200

    ready

--- goods.py
+++ goods.py
@@ -1,13 +1,13 @@
 # каталог товаров с возможностью добавления отзывов
 # наполнение каталога - название, стоимость в грн, категория
 categories = {
-    '1': 'science',
-    '2': 'novel',
-    '3': 'medicine',
+    '1': 'научно-популярная литература',
+    '2': 'беллетристика',
+    '3': 'медицина',
 }
 
 def input_category(prompt):
-    input_value = input(prompt)
+    input_value = input(prompt).strip()
     if input_value in categories:
         return input_value
     else:
@@ -15,7 +15,7 @@ def input_category(prompt):
         return input_category(prompt)
 
 def input_money(prompt):
-    input_value = input(prompt)
+    input_value = input(prompt).strip()
     try:
         return float(input_value)
     except ValueError:
@@ -23,7 +23,7 @@ def input_money(prompt):
         return input_money(prompt)
 
 def input_uniq(prompt, list):
-    input_value = input(prompt)
+    input_value = input(prompt).strip()
     if input_value not in list:
         return input_value
     else:
@@ -32,46 +32,49 @@ def input_uniq(prompt, list):
 
 class Goods():
 
-    list_names = []
+    list_id = []
+    list_goods = []
 
     def __init__(self, id, name, cost, category):
         self.id = id
         self.name = name
         self.cost = cost
         self.category = category
-        Goods.list_names.append(self.id)
+        Goods.list_id.append(self.id)
+        Goods.list_goods.append(self)
 
     @classmethod
     def new_good(cls):
-        good_id = input_uniq('Введите уникальный артикул: ', Goods.list_names)
+        good_id = input_uniq('Введите уникальный артикул: ', Goods.list_id)
         good_name = input('Введите название: ')
         good_cost = input_money('Введите стоимость в грн: ')
-        good_category = input_category('Введите категорию (1 для science, 2 для novel, 3 для medicine): ')
+        good_category = input_category('Введите категорию (1 для науч-поп., 2 для беллетристики, 3 для медицины): ')
         return cls(good_id, good_name, good_cost, good_category)
 
     def __repr__(self):
-        return 'Артикул: {}, название: "{}", {} (цена: {} грн)'.format(self.id, self.name, categories[self.category], self.cost)
+        return 'Артикул: {}, название: "{}", жанр: {} (цена: {} грн)'.format(self.id, self.name, categories[self.category], self.cost)
 
     @staticmethod
     def find_good():
-        input_value = input('Введите артикул товара: ')
-        if input_value in Goods.list_names:
+        input_value = input('Введите артикул товара: ').strip()
+        if input_value in Goods.list_id:
             return input_value
         else:
             print('Не существует такого артикула!')
-            return find_good()
+            return Goods.find_good()
 
-    def get_good(self, good_id_):
-        if self.id == good_id_:
-            return self
+    @staticmethod
+    def add_comment_id(good_id_):
+        for good in Goods.list_goods:
+            if good.id == good_id_:
+                good.self_add_comment()
 
-    def add_comment(self):
-        comment_name = input('Введите ваш отзыв: ')
+    def self_add_comment(self):
+        comment_name = input('Введите ваш отзыв: ').strip()
         return Comment(comment_name, self.id)
 
-    # def print_comments(self):
-    #     Comment.good == self:
-    #         print('Комментарий: ', Comment.name)
+    def self_add_comment_with_name(self, comment_name_):
+        return Comment(comment_name_, self.id)
 
 class Comment():
     list_comments = []
@@ -83,34 +86,21 @@ class Comment():
 
     @staticmethod
     def print_4_good(good_id_):
+        print('Комментарии: ')
+        count = 0
         for comment in Comment.list_comments:
             if comment.good_id == good_id_:
-                print('Комментарий: ', comment.name)
+                count += 1
+                print(count, ': ', comment.name)
             else:
-                print('Нет комментариев')
+                print('- отсутствуют')
+                break
 
-
-# good1 = Goods.new_good()
-# print(good1)
-good1 = Goods('11', 'good1', '100', '1')
-good2 = Goods('22', 'good2', '200', '2')
-good4 = Goods('332', 'good3', '200', '2')
-# good3 = Goods.new_good()
-# print(good3)
-
-print(Goods.find_good())
-# print(Goods.get_good(Goods.find_good()))
-
-# good1.add_comment()
-# good1.add_comment()
-# Comment.print_4_good(good1.id)
- # 3
-# good2.print_comments()
-
-
-# print_comments(good2, comment1)  #3
-
-
+good1 = Goods('1', 'good1', '100', '1')
+good2 = Goods('2', 'good2', '200', '2')
+good4 = Goods('3', 'good3', '300', '3')
+good1.self_add_comment_with_name('Жуткая книга')
+good1.self_add_comment_with_name('Ничего так книга')
 
 def main():
 
@@ -118,8 +108,9 @@ def main():
         print('''
         Меню:
         1) Добавить новый товар
-        2) Добавить комментарий к товару
-        3) Выход
+        2) Добавить комментарий к существующему товару
+        3) Распечатать все комментарии к существующему товару
+        4) Выход
         ''')
         choice = input('> ')
 
@@ -127,17 +118,19 @@ def main():
 
         if choice == '1':
             Goods.new_good()
-        elif choice == '2':
-            good_id = find_good()
-            get_good(good_id).add_comment()
+            print()
+            print('Вы создали новый товар:', '\n', Goods.list_goods[len(Goods.list_goods) - 1])
 
-            if
-                add_comment(self)
+        elif choice == '2':
+            Goods.add_comment_id(Goods.find_good())
 
         elif choice == '3':
+            print()
+            Comment.print_4_good(Goods.find_good())
+        elif choice == '4':
             break
         else:
-            print('Incorrect input')
+            print('Вы внесли неверный код!')
 
         print()
 
